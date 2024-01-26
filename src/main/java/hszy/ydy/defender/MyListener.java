1632c3958875c83d30fdeeae2dff4cddf98e52ac
ydy_dreemurr <ydy_dreemurr@outlook.com>
2024-01-26 10:58:58 +0800

    fixed bug that you can hold a protection4 in mainhand and a shield in offhand to trigger this plugin

--- src/main/java/hszy/ydy/defender/MyListener.java
+++ src/main/java/hszy/ydy/defender/MyListener.java
@@ -7,11 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class MyListener implements Listener {
 
@@ -20,21 +16,31 @@ public class MyListener implements Listener {
     {
         if(event.getEntity() instanceof Player)//是玩家
         {
-            //检测手里盾的附魔
-            if((((Player) event.getEntity()).getInventory().getItemInMainHand().getType() == Material.SHIELD)||(((Player) event.getEntity()).getInventory().getItemInOffHand().getType() == Material.SHIELD))
+            //受害者主手物品
+            ItemStack mainHandItem = ((Player) event.getEntity()).getInventory().getItemInMainHand();
+            //受害者副手物品
+            ItemStack offHandItem =  ((Player) event.getEntity()).getInventory().getItemInOffHand();
+
+            Player damagee = (Player) event.getEntity();
+
+            //检测被打的人主副手物品有无保护四附魔盾
+            if(((mainHandItem.getType() == Material.SHIELD && mainHandItem.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) == 4))||(offHandItem.getType() == Material.SHIELD && offHandItem.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) == 4))
             {
-                if((((Player) event.getEntity()).getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) == 4)||(((Player) event.getEntity()).getInventory().getItemInOffHand().getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) == 4))
+                //举盾完成
+                if(damagee.isBlocking())
                 {
-                    //举盾完成
-                    if(((Player) event.getEntity()).isBlocking() == true)
-                    {
-                        //必须挡住伤害
-                        if (event.getFinalDamage() == 0) {
-                            //反弹伤害
-                            ((Damageable) event.getDamager()).damage(event.getOriginalDamage(EntityDamageEvent.DamageModifier.BLOCKING) * Defender.dmg, event.getEntity());
-                            //破盾伤害
-                            if (Defender.undfd != 0)
-                                event.setDamage(event.getOriginalDamage(EntityDamageEvent.DamageModifier.BLOCKING) * Defender.undfd);
+                    //举盾的0.25秒之后才能挡住伤害
+                    if (event.getFinalDamage() == 0) {
+                        //打他的实体
+                        Damageable damager = (Damageable) event.getDamager();
+                        //反弹伤害
+                        damager.damage(event.getDamage() * Defender.dmg, damagee);
+                        //破盾伤害
+                        if (Defender.undfd != 0) {
+
+                            double finalHealth = (damagee.getHealth() - event.getDamage()) * Defender.undfd;
+                            if (finalHealth > 0)
+                                damagee.setHealth(finalHealth);
                         }
                     }
                 }
